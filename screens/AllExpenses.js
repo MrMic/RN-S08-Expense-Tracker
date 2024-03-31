b183f98ecedbd01189a881e05519d5a615ae02dd
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-03-31 12:20:23 +0200

    💫 feat: Add Screens Components Packages (RN/native + RNExpo + NativeStack + BottomTabs)

---
+++ screens/AllExpenses.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function AllExpenses() {
+  return (
+    <View>
+      <Text>AllExpenses Screen</Text>
+    </View>
+  );
+}
+
+export default AllExpenses;
