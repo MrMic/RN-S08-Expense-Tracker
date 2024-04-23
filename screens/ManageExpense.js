3d9539f6836dc704110880748f69b08739c1bf04
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-04-23 15:11:51 +0200

    💫 feat: Add "Delete" Button

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,7 @@
 import { useLayoutEffect } from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -11,11 +13,38 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
+  function deleteExpenseHandler() {}
+
   return (
-    <View>
-      <Text>ManageExpense Screen</Text>
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
     </View>
   );
 }
 
 export default ManageExpense;
+
+// ______________________________________________________________________
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
