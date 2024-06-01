d12c18597a413ed5115c32ae28f2604d38ba121e
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-06-01 13:03:11 +0200

    feat: Fetching Backend Data (GET Requests)

--- util/http.js
+++ util/http.js
@@ -1,8 +1,28 @@
 import axios from "axios";
 
+const BACKEND_URL =
+  "https://react-native-course-9316a-default-rtdb.europe-west1.firebasedatabase.app";
+
 export function storeExpense(expenseData) {
-  axios.post(
-    "https://react-native-course-9316a-default-rtdb.europe-west1.firebasedatabase.app/expenses.json",
-    expenseData,
-  );
+  axios.post(BACKEND_URL + "/expenses.json", expenseData);
+}
+
+export async function fecthExpenses() {
+  const response = await axios.get(BACKEND_URL + "/expenses.json");
+
+  const expenses = [];
+
+  // console.log("🪚 response.data:", response.data);
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      description: response.data[key].description,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+    };
+
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
 }
